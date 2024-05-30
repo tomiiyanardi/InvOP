9369313cbe9ee5b4455b094c06cea4e313baff34
tomiiyanardi <tomasyanardi@gmail.com>
2024-05-30 11:46:41 -0300

    articulo

---
+++ src/main/java/com/example/demo/entities/Articulo.java
@@ -0,0 +1,46 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "articulo")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class Articulo extends Base{
+
+    @Column(name = "costo_almacenamiento")
+    private float costo_almacenamiento;
+
+    @Column(name = "costo_orden")
+    private float costo_orden;
+
+    @Column(name = "lote_optimo")
+    private int lote_optimo;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "punto_pedido")
+    private int punto_pedido;
+
+    @Column(name = "stock_actual")
+    private int stock_actual;
+
+    @Column(name = "stock_seguridad")
+    private int stock_seguridad;
+
+    @Column(name = "tiempo_entre_pedidos")
+    private int tiempo_entre_pedidos;
+
+
+
+}
